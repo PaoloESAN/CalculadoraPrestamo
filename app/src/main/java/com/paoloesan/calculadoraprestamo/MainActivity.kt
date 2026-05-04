7752826c9d610bd0a033686c1d7370d945502980
Paolo <186249117+PaoloESAN@users.noreply.github.com>
2026-05-03 21:01:55 -0500

    navigator agregado

--- app/src/main/java/com/paoloesan/calculadoraprestamo/MainActivity.kt
+++ app/src/main/java/com/paoloesan/calculadoraprestamo/MainActivity.kt
@@ -4,13 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.paoloesan.calculadoraprestamo.navigation.Navigator
 import com.paoloesan.calculadoraprestamo.ui.theme.CalculadoraPrestamoTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +18,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CalculadoraPrestamoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Navigator()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CalculadoraPrestamoTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/com/paoloesan/calculadoraprestamo/navigation/Navigator.kt
@@ -0,0 +1,33 @@
+package com.paoloesan.calculadoraprestamo.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.paoloesan.calculadoraprestamo.viewModels.CalculadoraViewModel
+import com.paoloesan.calculadoraprestamo.screens.InicioScreen
+import com.paoloesan.calculadoraprestamo.screens.ResultadoScreen
+
+@Composable
+fun Navigator() {
+    val navController = rememberNavController()
+    val viewModel: CalculadoraViewModel = viewModel()
+
+    NavHost(navController = navController, startDestination = "input") {
+        composable("inicio") {
+            InicioScreen(
+                viewModel = viewModel
+            ) {
+                navController.navigate("resultado")
+            }
+        }
+        composable("resultado") {
+            ResultadoScreen(
+                viewModel = viewModel
+            ) {
+                navController.popBackStack()
+            }
+        }
+    }
+}
